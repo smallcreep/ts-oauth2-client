aacc5d6a6d05e0eababf494aa5e7f82ac463fc04
Ilia Rogozhin <ilia.rogozhin@gmail.com>
2019-08-07 07:40:10 +0300

    skeleton

--- README.md
+++ README.md
@@ -1,2 +1,27 @@
 # ts-oauth2-client
 OAuth2.0 client for browser and node.js on TypeScript
+
+Данная библиотека реализует взе известные методы описанные в 
+OAuth 2.0 Framework ([RFC 6749](https://tools.ietf.org/html/rfc6749)).
+Также библиотека работает одинаково как в браузерах, так и на серверной стороне в node.js.
+
+## Примеры использования
+
+### Authorization Code Grant
+
+It's implementation [section 4.1](https://tools.ietf.org/html/rfc6749#section-4.1) in RFC 6749.
+
+генерируем урл -> отправляем пользователя на этот урл -> получаем код -> запрашиваем токен
+
+```typescript
+new AuthorizationCodeGrant(
+    
+)
+```
+
+### Implicit Grant
+
+генерируем урл -> отправляем пользователя на этот урл -> получаем токен -> парсим его
+
+### Resource Owner Password Credentials Grant
+
---
+++ src/grants.ts
@@ -0,0 +1,70 @@
+/**
+ * An application making protected resource requests on behalf of the
+ * resource owner and with its authorization.  The term "client" does
+ * not imply any particular implementation characteristics (e.g.,
+ * whether the application executes on a server, a desktop, or other
+ * devices).
+ * @see https://tools.ietf.org/html/rfc6749#section-2.3.1
+ */
+interface Client {
+
+    /**
+     * The client identifier issued to the client during
+     * the registration process.
+     */
+    id: string,
+
+    /**
+     * The client secret. The client MAY omit the
+     * parameter if the client secret is an empty string.
+     */
+    secret: string,
+
+    /**
+     * Authorization string for HTTP Basic authentication scheme
+     * for authenticating clients that were issued a client password.
+     */
+    authorization(): string,
+}
+
+class Basic implements Client {
+
+    /**
+     * The client identifier issued to the client during
+     * the registration process.
+     */
+    id: string;
+
+    /**
+     * The client secret. The client MAY omit the
+     * parameter if the client secret is an empty string.
+     */
+    secret: string;
+
+    /**
+     * Ctor.
+     * @param id The client identifier
+     * @param secret The client secret
+     */
+    constructor(id: string, secret: string) {
+        this.id = id;
+        this.secret=secret;
+    }
+
+    /**
+     * Authorization string for HTTP Basic authentication scheme
+     * for authenticating clients that were issued a client password.
+     */
+    authorization(): string {
+        return "";
+    }
+}
+
+interface Grant {
+}
+
+class AuthorizationCodeGrant {
+    constructor() {
+    }
+}
+
